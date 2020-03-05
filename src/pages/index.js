6d2c880c8dfa18aa21ddae685b76902f4f4bfdf5
Pablo Ivan Maurig <maurigpi@dlatv.net>
2020-03-05 15:26:14 -0300

    agrego modal con videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 // components
 import Layout from '../components/common/layout'
@@ -8,8 +8,20 @@ import SectionAbout from '../components/home/sectionAbout'
 import { Grid, Divider, Container, Box, Typography } from '@material-ui/core'
 import siteConfig from '../data/siteConfig'
 import ImageQuery from '../components/common/imageQuery'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
 
 const IndexPage = () => {
+  const [open, setOpen] = useState(false)
+  const [videoId, setVideoId] = useState('')
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  const handleClose = () => {
+    setOpen(false);
+  }
+
   if (typeof window !== "undefined") {
     require("smooth-scroll")('a[href*="#"]', {
       offset: 64
@@ -49,21 +61,19 @@ const IndexPage = () => {
               </Grid>
               <Grid item xs={12} sm={9}>
                 <Grid container spacing={3} justify="center">
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
+                  {siteConfig.locucionWork.map((work, key) =>
+                    <Grid item xs={6} sm={4} key={key}>
+                      <div className='link' onClick={() => {
+                        setVideoId(work.id)
+                        handleOpen()
+                      }} onKeyPress={() => {
+                        setVideoId(work.id)
+                        handleOpen()
+                      }} role="button" tabIndex="0">
+                        <img src={`https://img.youtube.com/vi/${work.id}/0.jpg`} alt={work.name} />
+                      </div>
+                    </Grid>
+                  )}
                 </Grid>
               </Grid>
             </Grid>
@@ -129,24 +139,19 @@ const IndexPage = () => {
               </Grid>
               <Grid item xs={12} sm={9}>
                 <Grid container spacing={3} justify="center">
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
-                  <Grid item xs={6} sm={4}>
-                    <ImageQuery alt='barritas' className='img-responsive' imagen='barritas.jpg' />
-                  </Grid>
+                  {siteConfig.doblajeWork.map((work, key) =>
+                    <Grid item xs={6} sm={4} key={key}>
+                      <div className='link' onClick={() => {
+                        setVideoId(work.id)
+                        handleOpen()
+                      }} onKeyPress={() => {
+                        setVideoId(work.id)
+                        handleOpen()
+                      }} role="button" tabIndex="0">
+                        <img src={`https://img.youtube.com/vi/${work.id}/0.jpg`} alt={work.name} />
+                      </div>
+                    </Grid>
+                  )}
                 </Grid>
               </Grid>
             </Grid>
@@ -191,6 +196,15 @@ const IndexPage = () => {
           </Container>
         </Box>
       </StyleContainer>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogC id="alert-dialog-description">
+          <iframe title="modal" width="560" height="315" src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </DialogC>
+      </Dialog>
     </Layout >
   )
 }
@@ -200,5 +214,12 @@ const StyleContainer = styled.div`
     height: auto;
     
   }
+`
+const DialogC = styled(DialogContent)`
+    padding: 0!important;
+    iframe{
+      margin: 0;
+      margin-bottom: -7px;
+    }
 `
 export default IndexPage
